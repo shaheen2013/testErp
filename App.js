16cd073e3439c7592420c68cfbb15f4e7356163c
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2023-02-07 15:53:30 +0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import {  useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js/react-native';
+
+Pusher.logToConsole = true;
+
+
+const client = new Pusher("3", {
+  authEndpoint: "http://erp-back.test/broadcasting/auth",
+  cluster: "mt1",
+  wsHost: "erp-back.test",
+  wsPort: 6001,
+  enabledTransports: ["ws",'wss'],
+  forceTLS: false,
+  auth: {
+    headers: {
+      Authorization: 'Bearer XXXXXXXXXXXX',
+    }
+  },
+});
 
 export default function App() {
+  useEffect(() => {
+
+    const echo = new Echo({
+      broadcaster: "pusher",
+      client,
+      disableStats: true,      
+    });
+    console.log(echo)
+   }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Staring react native zahid ere</Text>
     </View>
   );
 }
